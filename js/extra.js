4bd8279311c5d506bd33231d7d587c5b972bf11e
Mustafa <mustafangure@gmail.com>
2018-09-28 10:48:57 +0300

    feat: Javascript files to test for linting

---
+++ js/extra.js
@@ -0,0 +1,13 @@
+/* eslint valid-jsdoc: "error"*/
+/* eslint-env es6*/
+
+/**
+ * get window height
+ * @returns {number} height of the window
+ */
+function getWindowHeight() {
+	let window = 0;
+	return window.innerHeight;
+}
+
+getWindowHeight();
---
+++ js/main.js
@@ -0,0 +1,4 @@
+(function() {
+	let foo = 12345;
+	return foo;
+})();
