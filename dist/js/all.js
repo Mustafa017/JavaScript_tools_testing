5e1eae1d2b087047c8de62c50606e611bf70bcd2
Mustafa <mustafangure@gmail.com>
2018-09-28 10:46:29 +0300

    feat: Production level distribution code

---
+++ dist/js/all.js
@@ -0,0 +1,18 @@
+/* eslint valid-jsdoc: "error"*/
+/* eslint-env es6*/
+
+/**
+ * get window height
+ * @returns {number} height of the window
+ */
+function getWindowHeight() {
+	let window = 0;
+	return window.innerHeight;
+}
+
+getWindowHeight();
+
+(function() {
+	let foo = 12345;
+	return foo;
+})();
